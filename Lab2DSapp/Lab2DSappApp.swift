83b22133ea44e7950959d8156ca342caba2d6e9e
Patryk Balinski <patrykbalinski00@gmail.com>
2024-04-27 14:23:01 +0200

    Initial Commit

---
+++ Lab2DSapp/Lab2DSappApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2DSappApp.swift
+//  Lab2DSapp
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Lab2DSappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
